b7ad2402a52c18239727a0bb3bb51cc045c918aa
Indio Ferrel <mferrel1911@gmail.com>
2022-05-16 11:48:54 -0700

    Signup function, no errors in console.

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
 // Enter Supabase Information
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://hndnxodsordjzcxmohou.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhuZG54b2Rzb3JkanpjeG1vaG91Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIzMDQ3MDEsImV4cCI6MTk2Nzg4MDcwMX0.d0akgji877V3JjW_u9QrFkhTnA5GHvZKs90nCkQ6V_w';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -8,7 +8,11 @@ export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
 
-export async function signupUser(email, password) {}
+export async function signupUser(email, password) {
+    const response = await client.auth.signUP({ email, password });
+
+    return response.user;
+}
 
 export async function signInUser(email, password) {}
 
